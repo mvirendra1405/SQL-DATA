7e7da1781c116c2229bc16c13a866722805cdc0d
mvirendra1405 <mvirendra1405@gmail.com>
2024-03-07 18:52:22 +0530

    'cc'

---
+++ sql lecture 4.1.sql
@@ -0,0 +1,7 @@
+select * from student;
+-- this is use for fetch the details
+select student.fname,student.contact from student;
+
+select fname,iname,contact from student;
+
+select fname as first_name ,iname as last_name,contact from student;s
