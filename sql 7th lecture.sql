93cd0c15d051624a026bd5e9f35acab8a2e451c4
mvirendra1405 <mvirendra1405@gmail.com>
2024-03-13 18:52:10 +0530

    '88';

---
+++ sql 7th lecture.sql
@@ -0,0 +1,13 @@
+select lower('roHit');
+select upper('itvedant');
+select concat('welcome to ITvedant' , ' THANE');
+select replace('welcome to ITvedant thane','thane','andheri');
+select replace('welcome to ITvedant thane','a','andheri');
+select length('My name is ram');
+select length(trim('      abc    abc      '));
+select trim('        abc      abc      ');
+select substring('My name is ram',1,2);
+select substring('My name is ram',12);
+
+select * from student_data;
+select upper(department) from student_data;
--- sql lecture 6.1.sql
+++ sql lecture 6.1.sql
@@ -5,3 +5,15 @@ insert into student_data value (15,'social','ddddd',55,35000,75);
 select max(marks) from student_data;
 select count(id),department from student_data group by department having avg(marks)>50;
 select avg(marks),department from student_data group by department;
+
+use virendra;
+show databases;
+select count(id),department from student_data group by department having count(department)>=2;
+select * from student_data where marks not in(45);
+select count(id),department from student_data where marks not in (45) group by department ;
+
+
+
+
+
+
